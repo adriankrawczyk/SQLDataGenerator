a764adc2642cd4931cb25c0eade3a03a57ccd3b2
adriankrawczyk <adriankrawczyk0@o2.pl>
2025-01-08 15:29:14 +0100

    english countries

--- students/generator.py
+++ students/generator.py
@@ -1,25 +1,4 @@
-def capitalize_first_letter(file_path):
-    try:
-        # Open the file and read all lines
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-        
-        # Ensure we don't lose content and capitalize each line safely
-        capitalized_lines = [line[:1].upper() + line[1:].lower() if line else '' for line in lines]
-        
-        # Write back the modified lines to the file
-        with open(file_path, 'w') as file:
-            file.writelines(capitalized_lines)
-        
-        print(f"File '{file_path}' has been updated successfully!")
-    
-    except FileNotFoundError:
-        print(f"Error: File '{file_path}' not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+countries = ['Polska', 'Niemcy', 'Francja', 'Belgia', 'Holandia', 'Czechy', 'Słowacja', 'Węgry', 'Austria', 'Szwajcaria', 'Włochy', 'Szwecja', 'Dania', 'Finlandia', 'Norwegia']
 
+countryID = ['PL', 'DE', 'FR', 'BE', 'NL', 'CZ', 'SK', 'HU', 'AT', 'CH', 'IT', 'SE', 'DK', 'FI', 'NO']
 
-# Example usage
-if __name__ == "__main__":
-    file_path = input("Enter the path to your text file: ")
-    capitalize_first_letter(file_path)
